f56e0222befb9b6377d10db329a0d3bddb9cb7b8
Alex Yaroshuk <alexander_4288@Yahoo.com>
2023-04-15 17:52:03 +0800

    update vercel.json and vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,9 @@ import vue from "@vitejs/plugin-vue";
 export default defineConfig({
   plugins: [vue()],
   server: {
-    historyApiFallback: {
-      rewrites: [{ from: /./, to: "/index.html" }],
-    },
     hot: true,
   },
   base: "/",
-  root: ".", // Change this line back to "."
-  publicDir: "public", // Add this line
+  root: ".",
+  publicDir: "public",
 });
