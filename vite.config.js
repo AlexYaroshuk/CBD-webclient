6c1fae7d9480eadf4f7f424cf5cf45fefbe7b11a
Alex Yaroshuk <alexander_4288@Yahoo.com>
2023-05-01 18:31:27 +0800

    modifying headers for svg handling

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,8 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-import copy from "vite-plugin-copy";
 
 export default defineConfig({
-  plugins: [
-    vue(),
-    copy({
-      targets: [
-        {
-          src: "public/assets/*",
-          dest: "dist/assets",
-        },
-      ],
-      hook: "writeBundle", // default
-    }),
-  ],
+  plugins: [vue()],
   server: {
     hot: true,
   },
